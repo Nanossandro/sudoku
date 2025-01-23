f40dd7c728b7e807014565bedd3c74f4daae614d
Nanossandro <148973177+Nanossandro@users.noreply.github.com>
2025-01-23 12:21:02 +0100

    debut proghrammem

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import numpy as np
+import pygame
+import random as rd
+pygame.init()
 
 
-print ("hello wrld")
 
-def tst():
-    print("hello")
 
-print (tst())
-   
+def creatboard():
+
+    for i in range (9):
+        for j in range (9):
+            print ("_|")
+
+
+creatboard()
